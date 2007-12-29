9e70b8a5b8a694de8e3dc78af6ea2699cdc30dfd
Rudolf.Noe <Rudolf.Noe@46940e5f-6c42-0410-b4e4-d3c52341906d>
2007-12-29 13:05:36 +0000

    Initial import

---
+++ tags/0.4.2/src/defaults/preferences/mlb_prefs.js
@@ -0,0 +1,29 @@
+/**
+ * Default-Prefs for MLB
+ * Rudolf Noe
+ * 27.12.05
+ */
+pref("mouselessbrowsing.autoExecuteDelay", "500");
+pref("mouselessbrowsing.disableAllIds", false);
+pref("mouselessbrowsing.enableFrameIds", true);
+pref("mouselessbrowsing.enableImgLinkIds", true);
+pref("mouselessbrowsing.enableLinkIds", true);
+pref("mouselessbrowsing.enableFormElementIds", true);
+pref("mouselessbrowsing.exclusiveNumpad", false);
+pref("mouselessbrowsing.executeAutomatic", false);
+pref("mouselessbrowsing.keys.clearKeybuffer", "432");
+pref("mouselessbrowsing.keys.historyBack", "1776");
+pref("mouselessbrowsing.keys.historyForward", "1696");
+pref("mouselessbrowsing.keys.openInNewTabPostfixKey", "1712");
+pref("mouselessbrowsing.keys.selectLink", "260");
+pref("mouselessbrowsing.keys.scrollDown", "1712");
+pref("mouselessbrowsing.keys.scrollUp", "1744");
+pref("mouselessbrowsing.keys.toggleAllIds", "1762");
+pref("mouselessbrowsing.keys.toggleMLB", "1760");
+pref("mouselessbrowsing.keys.toggleExlusiveUseOfNumpad", "2304");
+pref("mouselessbrowsing.pixelsToScroll", "150");
+pref("mouselessbrowsing.showKeybufferInStatusbar", true);
+pref("mouselessbrowsing.showIdsOnDemand", false);
+pref("mouselessbrowsing.styleForFrameIdSpan", "z-index:500;font-size:xx-small;font-family:sans-serif;background-color:D6E3FE;border-color:black; border-width:1px;border-style:solid;padding:0px 3px 0px 3px; position:absolute; top:0; left:0;");
+pref("mouselessbrowsing.styleForIdSpan", "float:none;margin-left:3px; z-index:500;font-size:xx-small;font-family:sans-serif;border-color:grey; border-width:1px;border-style:solid;padding:0px 3px 0px 3px;");
+
