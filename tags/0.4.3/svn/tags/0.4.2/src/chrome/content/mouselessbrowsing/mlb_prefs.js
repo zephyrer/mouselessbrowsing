60d07417c2fad5bf433cf45e82ff8681dc93b204
Rudolf.Noe <Rudolf.Noe@46940e5f-6c42-0410-b4e4-d3c52341906d>
2007-12-30 10:33:00 +0000

    Copied remotely

---
+++ tags/0.4.3/svn/tags/0.4.2/src/chrome/content/mouselessbrowsing/mlb_prefs.js
@@ -0,0 +1,33 @@
+function MLB_doOnload(){
+	//MLB_updateInterval();
+	MLB_onTogglingVisibilityAllIds();
+}
+
+function MLB_updateInterval(){
+	var showIds = document.getElementById('showIdsDuringLoadProcess').checked;
+	var intervalField = document.getElementById('updateIntervalOnLoad');
+	if(showIds){
+		intervalField.disabled=false;
+		intervalField.value = gPref.getCharPref('mouselessbrowsing.updateIntervalOnLoad');
+	}else{
+		intervalField.disabled=true;
+		intervalField.value="";
+	}
+}
+
+function MLB_checkUpdateInterval(){
+	var updateIntervalOnLoad = document.getElementById('"updateIntervalOnLoad').value;
+	if(isNaN(updateIntervalOnLoad))
+		return;
+	updateIntervalOnLoad = parseInt(updateIntervalOnLoad, 10);
+	if(updateIntervalOnLoad<700)
+		alert('Update intervals smaller than 700 msec result in an significant performance overhead');
+}
+
+function MLB_onTogglingVisibilityAllIds(){
+	var disableVisibilityFlags = document.getElementById("allIds").checked;
+	var visibilityFlags = document.getElementsByAttribute ( "visibilityFlag", "true" );
+	for(var i=0; i<visibilityFlags.length; i++){
+		visibilityFlags[i].disabled=disableVisibilityFlags;
+	}
+}
