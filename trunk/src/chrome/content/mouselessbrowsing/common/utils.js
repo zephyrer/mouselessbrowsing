588c352da35fb781eff32dc2670885d1699cafe5
Rudolf.Noe <Rudolf.Noe@46940e5f-6c42-0410-b4e4-d3c52341906d>
2007-12-30 17:22:55 +0000

    Initial import of new version mlb

---
+++ trunk/src/chrome/content/mouselessbrowsing/common/utils.js
@@ -0,0 +1,60 @@
+var MLB_Utils = {
+    prefs: Components.classes["@mozilla.org/preferences-service;1"].
+                getService(Components.interfaces.nsIPrefBranch),
+
+    getCharPref: function(key){
+        return MLB_Utils.prefs.prefHasUserValue(key)?MLB_Utils.prefs.getCharPref(key):null;
+    },
+    
+    getBoolPref: function(key){
+        return MLB_Utils.prefs.prefHasUserValue(key)?MLB_Utils.prefs.getBoolPref(key):false;
+    },
+
+    hasUserPref: function(key){
+    	return MLB_Utils.prefs.prefHasUserValue(key);
+   	},
+   	setCharPref: function(key, value){
+   		MLB_Utils.prefs.setCharPref(key, value);
+   	},
+    /*
+     * Returns true when the srcElement of the keyevent is an textfield, password-field
+     * selectbox or textarea
+     */
+    isWritableElement: function(element){
+        if(element==null || element.tagName==null)
+            return false;
+        var tagName = element.tagName.toUpperCase();
+        var type = element.type?element.type.toUpperCase():"";
+        var isWritableFormElement = (tagName.indexOf("INPUT")!=-1 && (type=="TEXT" || 
+                type=="PASSWORD")) || tagName.indexOf("TEXTAREA")!=-1 || 
+                tagName.indexOf("SELECT")!=-1;
+        return isWritableFormElement;
+    },
+        
+    isTagName: function(element, tagName){
+        if(!element || !element.tagName)
+            return false;
+        return element.tagName.toUpperCase()==tagName.toUpperCase();
+    },
+    
+    isEmptyTextNode: function(element){
+        if(element.nodeType==Node.TEXT_NODE && element.nodeValue=="")
+            return true
+        else
+            return false;
+    },
+    
+    logMessage: function (aMessage) {
+        var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
+                       .getService(Components.interfaces.nsIConsoleService);
+        consoleService.logStringMessage(aMessage);
+    },
+    
+    logError: function(error){
+        var errorMessage = "";
+        for(e in error){
+            errorMessage = errorMessage + e + ": " + error[e] + "\n";
+        }
+        MLB_Utils.logMessage(errorMessage);
+    }
+}
