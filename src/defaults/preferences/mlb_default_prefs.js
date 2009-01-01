07eec3c1bd5172f5a680aec1a30ef41e3bbb7762
Rudolf.Noe <Rudolf.Noe@46940e5f-6c42-0410-b4e4-d3c52341906d>
2009-01-01 10:11:36 +0000

    Precommit MLB 0.5.2Beta1

--- src/defaults/preferences/mlb_default_prefs.js
+++ src/defaults/preferences/mlb_default_prefs.js
@@ -49,6 +49,7 @@ pref("mouselessbrowsing.keys.blurActiveElement", "0"); //None
 pref("mouselessbrowsing.keys.blockKeyboardInputForMlb", "0"); //None
 pref("mouselessbrowsing.keys.toggleAllIds", "1762"); //Crtl+Decimal
 pref("mouselessbrowsing.keys.toggleMLB", "1760"); //Decimal
+pref("mouselessbrowsing.keys.updatePage", "1713"); //Alt+Add
 pref("mouselessbrowsing.keys.toggleExlusiveUseOfNumpad", "304"); //Pause
 pref("mouselessbrowsing.keys.openConfig", "1239"); //Ctrl+Alt+Shift+M
 pref("mouselessbrowsing.keys.addSiteRule", "1319");//Ctrl+Alt+Shift+R
